8259d90fafa31f79485c5f80c5cf0e4d58c16a28
Ryan Gehris <rogehris@gmail.com>
2023-03-01 10:44:25 -0800

    added folder and files for product-list component

--- paige-denim/src/app/app.module.ts
+++ paige-denim/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
