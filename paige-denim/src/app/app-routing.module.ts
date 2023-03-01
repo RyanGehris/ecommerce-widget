30b9488403f5bf2e123d495e143000452c5d4749
Ryan Gehris <rogehris@gmail.com>
2023-03-01 10:48:57 -0800

    added routes to ProductList and ProductDetail components

--- paige-denim/src/app/app-routing.module.ts
+++ paige-denim/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ProductListComponent', component: ProductListComponent },
+  { path: 'ProductDetailComponent', component: ProductDetailComponent },
+  { path: '**', redirectTo: '/home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
