c4fc3e87afee098a627b2b536efadbfb52e8e82c
Ryan Gehris <rogehris@gmail.com>
2023-03-02 19:04:40 -0800

    added styling changes to both product-list and product-detail component

--- paige-denim/src/app/app.module.ts
+++ paige-denim/src/app/app.module.ts
@@ -9,15 +9,17 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent, ProductDetailComponent],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     FormsModule,
+    HttpClientModule,
+    MatSelectModule,
     MaterialModule,
   ],
   providers: [HttpClientModule],
